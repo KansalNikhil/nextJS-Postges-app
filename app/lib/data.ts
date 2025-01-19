1c61b7eca859a3af9203c5da1038de7d61097884
Nikhil Kansal <nikhilkansal09@gmail.com>
2025-01-19 17:16:17 +0530

    dashboardErrorFix

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -19,10 +19,9 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    // const client = await db.connect();
+    const client = await db.connect();
 
-    // const data = await client.sql<Revenue>`SELECT * FROM revenue`;
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
+    const data = await client.sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
 
@@ -35,10 +34,9 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    // const client = await db.connect();
+    const client = await db.connect();
 
-    // const data = await client.sql<LatestInvoiceRaw>`
-    const data = await sql<LatestInvoiceRaw>`
+    const data = await client.sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
@@ -61,11 +59,11 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
-    // const client = await db.connect();
+    const client = await db.connect();
 
-    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
-    const invoiceStatusPromise = sql`SELECT
+    const invoiceCountPromise = client.sql`SELECT COUNT(*) FROM invoices`;
+    const customerCountPromise = client.sql`SELECT COUNT(*) FROM customers`;
+    const invoiceStatusPromise = client.sql`SELECT
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
